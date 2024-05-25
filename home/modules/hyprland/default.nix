213c94ae311d2ac187344952e1e031e8524f226e
Laxa <laxamore@gmail.com>
2024-05-26 02:24:31 +0700

    hyprland waybar

--- home/modules/hyprland/default.nix
+++ home/modules/hyprland/default.nix
@@ -1,30 +1,6 @@
-{ config, ... }:
-
 {
-  home = {
-    sessionVariables = {
-      EDITOR = "lvim";
-      BROWSER = "brave";
-      TERMINAL = "kitty";
-      GBM_BACKEND= "nvidia-drm";
-      __GLX_VENDOR_LIBRARY_NAME= "nvidia";
-      LIBVA_DRIVER_NAME= "nvidia"; # hardware acceleration
-      __GL_VRR_ALLOWED="1";
-      WLR_NO_HARDWARE_CURSORS = "1";
-      WLR_RENDERER_ALLOW_SOFTWARE = "1";
-      CLUTTER_BACKEND = "wayland";
-      WLR_RENDERER = "vulkan";
-
-      XDG_CURRENT_DESKTOP = "Hyprland";
-      XDG_SESSION_DESKTOP = "Hyprland";
-      XDG_SESSION_TYPE = "wayland";
-    };
-  };
-
-  home.file.".config/hypr/hyprland.conf".source = config.lib.file.mkOutOfStoreSymlink ./hyprland.conf;
-
   wayland.windowManager.hyprland = {
     enable = true;
-    # extraConfig = (builtins.readFile ./hyprland.conf);
+    extraConfig = (builtins.readFile ./hyprland.conf);
   };
 }
--- nixos/modules/hyprland/default.nix
+++ nixos/modules/hyprland/default.nix
@@ -3,6 +3,22 @@
 {
   environment.sessionVariables = rec {
     NIXOS_OZONE_WL="y";
+    EDITOR = "nvim";
+    BROWSER = "brave";
+    TERMINAL = "kitty";
+
+    #GBM_BACKEND= "nvidia-drm";
+    #__GLX_VENDOR_LIBRARY_NAME= "nvidia";
+    #LIBVA_DRIVER_NAME= "nvidia"; # hardware acceleration
+    #__GL_VRR_ALLOWED="1";
+    #WLR_NO_HARDWARE_CURSORS = "1";
+    #WLR_RENDERER_ALLOW_SOFTWARE = "1";
+    #CLUTTER_BACKEND = "wayland";
+    #WLR_RENDERER = "vulkan";
+
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_DESKTOP = "Hyprland";
+    XDG_SESSION_TYPE = "wayland";
   };
 
   environment.systemPackages = with pkgs; [
@@ -22,7 +38,7 @@
 
     hyprland = {
       enable = true;
-      #xwayland.enable = true;
+      xwayland.enable = true;
     };
 
     dconf.enable = true;
